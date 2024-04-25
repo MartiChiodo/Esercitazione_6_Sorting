36249d653a26805b9307f34b0bfa01828b71a224
martina <s293874@studenti.polito.it>
2024-04-25 10:10:39 +0200

    Initial commit

---
+++ Esercizio_2/main.cpp
@@ -0,0 +1,18 @@
+/*
+Questa esercitazione ha lo scopo di confrontare le performances dei vettori di ordinamento che abbiamo studianto. Per fare ciò
+possiamo confrontare i tempi medi con cui girano i due algoritmi in alcuni study cases:
+    * vettori ordinati
+    * vettori ordinati al contrario
+    * vettori ordinati che hanno subito alcune perturbazioni legati solo ad alcuni elementi
+    * vettori di diverse dimensioni
+
+--> Fare attenzione che se passo un vettore v ad un algoritmo questo me lo ordinerà quindi non posso passarlo anche all'altro
+    algoritmo; dovrò creare due copie dello stesso vettore e passare una copia ad un algoritmo e l'altro all'altro algoritmo.
+ */
+
+
+int main()
+{
+    return 0;
+}
+
