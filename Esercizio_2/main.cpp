864469dc6300b677a72c8be203f102c2a729d093
martina <s293874@studenti.polito.it>
2024-04-28 15:58:38 +0200

    some fix

--- Esercizio_2/main.cpp
+++ Esercizio_2/main.cpp
@@ -8,9 +8,25 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 using namespace std;
 using namespace SortLibrary;
+
+string generateRandomChar(const unsigned int len)
+    {
+    const int MAX = 26;
+    char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+
+    string res = "";
+    for (int i = 0; i < len; i++)
+        res = res + alphabet[rand() % MAX];
+
+    return res;
+    }
 
 double average(vector<double>& v){
     double sum = accumulate(v.begin(), v.end(), 0);
@@ -18,7 +34,8 @@ double average(vector<double>& v){
     return media;
 }
 
-double durata_BubbleSort (vector<int>& v){
+template<typename T>
+double durata_BubbleSort (vector<T>& v){
 
     chrono::steady_clock::time_point t0 = chrono::steady_clock::now();
     BubbleSort(v);
@@ -28,7 +45,8 @@ double durata_BubbleSort (vector<int>& v){
     return durata;
 }
 
-double durata_MergeSort (vector<int>& v){
+template<typename T>
+double durata_MergeSort (vector<T>& v){
 
     chrono::steady_clock::time_point t0 = chrono::steady_clock::now();
     MergeSort(v);
@@ -48,7 +66,7 @@ int main(int argc, char ** argv)
     cout << scientific << setprecision(4); //imposto il formato con cui visualizzerò in output i tempi medi
 
 // *******************************************************************************************************************************
-    //misurazioni con vettore ordinato in maniera crescente
+    //misurazioni con vettore di interi ordinato in maniera crescente
     vector<double> misurazioni_BS_ordinato1(num_misurazioni);
     vector<double> misurazioni_MS_ordinato1(num_misurazioni);
     misurazioni_BS_ordinato1.reserve(num_misurazioni);
@@ -64,7 +82,7 @@ int main(int argc, char ** argv)
         misurazioni_MS_ordinato1[i] = durata_MergeSort(v2);
     }
 
-    cout << "Misurazioni con vettore ordinato in maniera crescente di dimensione " << dim_vettore << "." << endl;
+    cout << "Misurazioni con vettore di interi ordinato in maniera crescente di dimensione " << dim_vettore << "." << endl;
     cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_ordinato1) << " microseconds." << endl;
     cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_ordinato1) << " microseconds." << endl;
 
@@ -72,7 +90,7 @@ int main(int argc, char ** argv)
     cout << endl;
 
 // *******************************************************************************************************************************
-    //misurazioni con vettore ordinato in maniera decrescetnte
+    //misurazioni con vettore di interi ordinato in maniera decrescetnte
     vector<double> misurazioni_BS_ordinato2(num_misurazioni);
     vector<double> misurazioni_MS_ordinato2(num_misurazioni);
     misurazioni_BS_ordinato2.reserve(num_misurazioni);
@@ -89,14 +107,14 @@ int main(int argc, char ** argv)
         misurazioni_MS_ordinato2[i] = durata_MergeSort(v2);
     }
 
-    cout << "Misurazioni con vettore ordinato in maniera decrescente di dimensione " << dim_vettore << "." << endl;
+    cout << "Misurazioni con vettore di interi ordinato in maniera decrescente di dimensione " << dim_vettore << "." << endl;
     cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_ordinato2) << " microseconds." << endl;
     cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_ordinato2) << " microseconds." << endl;
 
     cout << endl;
 
 // *******************************************************************************************************************************
-    //misurazioni con vettore per metà ordinato
+    //misurazioni con vettore di interi per metà ordinato
     vector<double> misurazioni_BS_semiordinato(num_misurazioni);
     vector<double> misurazioni_MS_semiordinato(num_misurazioni);
     misurazioni_BS_semiordinato.reserve(num_misurazioni);
@@ -122,14 +140,14 @@ int main(int argc, char ** argv)
         misurazioni_MS_semiordinato[i] = durata_MergeSort(v2);
     }
 
-    cout << "Misurazioni con vettore ordinato per metà di dimensione " << dim_vettore << "." << endl;
+    cout << "Misurazioni con vettore di interi ordinato per metà di dimensione " << dim_vettore << "." << endl;
     cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_semiordinato) << " microseconds." << endl;
     cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_semiordinato) << " microseconds." << endl;
 
     cout << endl;
 
 // *******************************************************************************************************************************
-    //misurazioni con vettore non ordinato
+    //misurazioni con vettore di interi non ordinato
     vector<double> misurazioni_BS_nonordinato(num_misurazioni);
     vector<double> misurazioni_MS_nonordinato(num_misurazioni);
     misurazioni_BS_nonordinato.reserve(num_misurazioni);
@@ -152,11 +170,41 @@ int main(int argc, char ** argv)
         misurazioni_MS_nonordinato[i] = durata_MergeSort(v2);
     }
 
-    cout << "Misurazioni con vettore non ordinato di dimensione " << dim_vettore << "." << endl;
+    cout << "Misurazioni con vettore di interi non ordinato di dimensione " << dim_vettore << "." << endl;
     cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_nonordinato) << " microseconds." << endl;
     cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_nonordinato) << " microseconds." << endl;
 
     cout << endl;
+
+// *******************************************************************************************************************************
+    //misurazioni con vettore di stringhe non ordinato
+    vector<double> misurazioni_BS_str(num_misurazioni);
+    vector<double> misurazioni_MS_str(num_misurazioni);
+    misurazioni_BS_str.reserve(num_misurazioni);
+    misurazioni_MS_str.reserve(num_misurazioni);
+
+    const unsigned int len = 10; //imposto la lunghezza delle stringhe che voglio ordinare
+
+    for (unsigned int i = 0; i < num_misurazioni; i++){
+        vector<string> v1(dim_vettore);
+        vector<string> v2(dim_vettore);
+
+        for (unsigned int k = 0; k < dim_vettore; k++){
+            v1[k] = generateRandomChar(len);
+        }
+
+        v2 = v1;
+
+        misurazioni_BS_str[i] = durata_BubbleSort(v1);
+        misurazioni_MS_str[i] = durata_MergeSort(v2);
+    }
+
+    cout << "Misurazioni con vettore di stringhe non ordinato di dimensione " << dim_vettore << "." << endl;
+    cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_str) << " microseconds." << endl;
+    cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_str) << " microseconds." << endl;
+
+    cout << endl;
+
 
 // *******************************************************************************************************************************
     //misurazioni con vettore non ordinato di dimensione raddoppiata
@@ -184,43 +232,13 @@ int main(int argc, char ** argv)
         misurazioni_MS_doppiadim[i] = durata_MergeSort(v2);
     }
 
-    cout << "Misurazioni con vettore non ordinato di dimensione " << dim << "." << endl;
+    cout << "Misurazioni con vettore di interi non ordinato di dimensione " << dim << "." << endl;
     cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_doppiadim) << " microseconds." << endl;
     cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_doppiadim) << " microseconds." << endl;
 
     cout << endl;
 
-// *******************************************************************************************************************************
-    //misurazioni con vettore non ordinato di dimensione molto piccola
-    vector<double> misurazioni_BS_piccoladim(num_misurazioni);
-    vector<double> misurazioni_MS_piccoladim(num_misurazioni);
-    misurazioni_BS_piccoladim.reserve(num_misurazioni);
-    misurazioni_MS_piccoladim.reserve(num_misurazioni);
 
-    dim = 10;
-
-    srand((unsigned) time(NULL)); //imposto il seed per ottenere numeri generati casualmente
-
-    for (unsigned int i = 0; i < num_misurazioni; i++){
-        vector<int> v1(dim);
-        vector<int> v2(dim);
-
-        //genero l'intero vettore casualmente nell'intervallo [0; 100]
-        for (unsigned int k = 0; k < dim; k++){
-            v1[k] = rand() % 101;
-        }
-
-        v2 = v1;
-
-        misurazioni_BS_piccoladim[i] = durata_BubbleSort(v1);
-        misurazioni_MS_piccoladim[i] = durata_MergeSort(v2);
-    }
-
-    cout << "Misurazioni con vettore non ordinato di dimensione " << dim << "." << endl;
-    cout << "L'algoritmo Bubble Sort impiega mediamente " << average(misurazioni_BS_piccoladim) << " microseconds." << endl;
-    cout << "L'algoritmo Merge Sort impiega mediamente " << average(misurazioni_MS_piccoladim) << " microseconds." << endl;
-
-    cout << endl;
     return 0;
 }
 
